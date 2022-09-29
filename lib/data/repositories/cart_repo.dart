058701bb27b8ce93c3624f6fac27b7305a729497
ASM_Fahim <asmfahim1@gmail.com>
2022-09-29 12:38:55 +0600

    2 hours, 11 mins, 35 secs

--- lib/data/repositories/cart_repo.dart
+++ lib/data/repositories/cart_repo.dart
@@ -1,1 +1,20 @@
-class CartRepo {}
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../models/cart_model.dart';
+
+class CartRepo {
+  final SharedPreferences sharedPreferences;
+
+  CartRepo({required this.sharedPreferences});
+
+  List<String> cart = [];
+
+  void addToCartList(List<CartModel> cartList) {
+    cart = [];
+    cartList.forEach((element) {
+      cart.add(jsonEncode(element));
+    });
+  }
+}
--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -2,7 +2,12 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:food_apps/helper/routes/route_helper.dart';
+import 'package:food_apps/utils/dimensions.dart';
 import 'package:get/get.dart';
+
+import '../../controllers/cart_controller.dart';
+import '../../controllers/popular_product_controller.dart';
+import '../../controllers/recommended_product_controller.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -16,22 +21,36 @@ class _SplashPageState extends State<SplashScreen>
   late Animation<double> animation;
   late AnimationController controller;
 
+  Future<void> _loadResources() async {
+    await Get.find<PopularProductController>().getPopularProductList();
+    await Get.find<RecommendedProductController>().getRecommendedProductList();
+    Get.find<CartController>();
+  }
+
   @override
   void initState() {
+    _loadResources();
     controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 3),
     )..forward();
     animation = CurvedAnimation(
       parent: controller,
       curve: Curves.linear,
     );
-    Timer(const Duration(seconds: 2), () {
+    Timer(const Duration(seconds: 3), () {
       Get.offNamed(RouteHelper.getInitial());
     });
 
     // TODO: implement initState
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -45,8 +64,8 @@ class _SplashPageState extends State<SplashScreen>
             scale: animation,
             child: Center(
               child: Image.asset(
-                'assets/images/hospitals.jpg',
-                width: 250,
+                'assets/images/splLogo.png',
+                width: Dimensions.splashImg,
               ),
             ),
           ),
